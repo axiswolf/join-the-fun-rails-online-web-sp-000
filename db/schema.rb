cf63780e12b38e60f6fdf6a6807b90cb380bda84
Karen V <karenvo@usc.edu>
2019-12-03 09:59:43 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205318) do
-
-  create_table "passengers", force: :cascade do |t|
-    t.integer  "ride_id"
-    t.integer  "taxi_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20140212205242) do
 
   create_table "rides", force: :cascade do |t|
     t.integer  "ride_id"
